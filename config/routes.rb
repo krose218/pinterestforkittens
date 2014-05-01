0a2128f6143c7af85d179b227c2c670b6d750239
Kelsey Freedman <kfreedma@bates.edu>
2014-05-01 17:21:02 -0400

    More Database greatness

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pinterestforkittens::Application.routes.draw do
+  get '/about' => "page#about"
   root "page#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
